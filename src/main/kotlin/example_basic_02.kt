a2b461411119a1806aa8312187c657d17b89a0c9
tmdgusya <tmdgusya@naver.com>
2022-05-29 19:44:57 +0900

    코루틴 Example01 - RunBlocking 을 통해서 MainThread Blocking

---
+++ src/main/kotlin/example_basic_02.kt
@@ -0,0 +1,13 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    GlobalScope.launch {
+        delay(1000)
+        println("World")
+    }
+    println("Hello, ")
+    delay(1000)
+}
