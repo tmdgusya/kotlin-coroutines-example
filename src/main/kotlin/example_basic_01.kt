cb240765208498e05de16aa203f3d37c9c381400
tmdgusya <tmdgusya@naver.com>
2022-05-29 19:36:33 +0900

    코루틴 Example01 - Thread Ver

--- src/main/kotlin/example_basic_01.kt
+++ src/main/kotlin/example_basic_01.kt
@@ -5,7 +5,9 @@ fun main() {
 
     /**
      * GlobalScope 는 안을보면 SingleObjcet 로 GlobalScope
+     * Coroutines -> Thread 로 바꿔도 상관없음 단순 light-weight thread 이다.
      */
+    // GlobalScope.launch
     thread {
         sleep(1000)
         println("World") // non-blocking main thread
