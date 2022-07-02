dc8cb89153ed3ef0e8cf8e7ab8b48c081fea5179
tmdgusya <tmdgusya@naver.com>
2022-07-02 22:52:40 +0900

    Coroutine Cancellation

--- src/main/kotlin/cancellation/Example01.kt
+++ src/main/kotlin/cancellation/Example01.kt
@@ -1,2 +1,19 @@
 package cancellation
 
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+suspend fun main() = coroutineScope {
+    val job = launch {
+        repeat(1_000) { i ->
+            delay(100)
+            Thread.sleep(100) // We simulate long operation
+            println("Printing $i")
+        }
+    }
+    delay(1000)
+    job.cancelAndJoin()
+    println("Cancelled successfully")
+}
