593e2578df0c06de49b6fce6bddae971da05144d
tmdgusya <tmdgusya@naver.com>
2022-07-06 23:14:53 +0900

    Coroutine Exception Handling

--- src/main/kotlin/coroutine_action/Example02.kt
+++ src/main/kotlin/coroutine_action/Example02.kt
@@ -12,16 +12,20 @@ val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
 
 suspend fun main() {
     CoroutineScope(Dispatchers.IO).launch(exceptionHandler) {
+        println("Current Thread : ${Thread.currentThread().name}")
         val a = launch {
+            println("Current Thread A : ${Thread.currentThread().name}")
             delay(1000)
             throw RuntimeException("Test")
             10
         }
         println("a is calculated")
         val b = launch {
+            println("Current Thread B : ${Thread.currentThread().name}")
             delay(1000)
             20
         }
+        println("Current Thread : ${Thread.currentThread().name}")
         println(a) // 10
         println(b) // 20
     }.join()
